5297d1cb89220eb37ff9fc8ddec4cd5f6368500f
unknown <stefu1234@gmail.com>
2019-05-10 12:58:34 -0700

    first commit, adding the game

---
+++ app/src/main/java/com/example/catchball/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.catchball;
+
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //Frame
+    private FrameLayout gameFrame;
+    private int frameHeight, frameWidth, initialFrameWidth;
+    private LinearLayout startLayout;
+
+
+    //Image
+    private ImageView  box, black, pink, orange;
+    private Drawable imageBoxRight, imageBoxLeft;
+
+    //Size
+    private int boxSize;
+
+    //Position
+
+    private float boxX,boxY;
+    private float blackX, blackY;
+    private float pinkX, pinkY;
+    private float orangeX, orangeY;
+
+    //Score
+    private TextView scoreLabel, highScoreLabel;
+    private int score, highScore, timeCount;
+
+    //Class
+    private Timer timer;
+    private Handler handler= new Handler();
+
+    //Status
+    private boolean start_flg = false;
+    private boolean action_flg = false;
+    private boolean pink_flg = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gameFrame = findViewById(R.id.gameFrame);
+        startLayout= findViewById(R.id.startLayout);
+        box = findViewById(R.id.box);
+        black = findViewById(R.id.black);
+        pink = findViewById(R.id.pink);
+        orange = findViewById(R.id.orange);
+        scoreLabel = findViewById(R.id.scoreLabel);
+        highScoreLabel = findViewById(R.id.highScoreLabel);
+
+        imageBoxLeft = getResources().getDrawable(R.drawable.box_left);
+        imageBoxRight = getResources().getDrawable(R.drawable.box_right);
+
+    }
+
+    public void changePos(){
+
+    }
+
+    public void startGame(View view){
+        start_flg= true;
+        startLayout.setVisibility(View.INVISIBLE);
+        if (frameHeight == 0){
+            frameHeight = gameFrame.getHeight();
+            frameWidth = gameFrame.getWidth();
+            initialFrameWidth = frameWidth;
+
+            boxSize = box.getHeight();
+            boxX = box.getX();
+            boxY = box.getY();
+        }
+
+        box.setX(0.0f);
+        black.setY(3000.0f);
+        orange.setY(3000.0f);
+        pink.setY(3000.0f);
+
+        blackY = black.getY();
+        orangeY = orange.getY();
+        pinkY = pink.getY();
+
+        box.setVisibility(View.VISIBLE);
+        black.setVisibility(View.VISIBLE);
+        orange.setVisibility(View.VISIBLE);
+        pink.setVisibility(View.VISIBLE);
+
+
+        timeCount = 0;
+        score = 0;
+
+        scoreLabel.setText("Score : 0");
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (start_flg){
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            }
+        },0, 20 );
+
+
+
+    }
+
+    public void endGame(View view){
+
+
+    }
+
+
+
+
+
+}
